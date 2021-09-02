528ec737c268a1401e76904f241299ca1cbec531
Sergio E. Prellezo <sereprec@gmail.com>
2021-09-02 17:56:34 -0300

    Agrego logo al header con efecto scroll. Inicio responsive en header y footer

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const $carouselLogo = document.getElementById("carousel__logo");
+const $headerLogo = document.getElementById("header__logo");
+const $navbar = document.querySelector("header nav.nav");
+
+let logoVisibility = isVisible($carouselLogo);
+
+function isVisible(elem) {
+  let coords = elem.getBoundingClientRect();
+  let windowHeight = document.documentElement.clientHeight;
+  let bottomVisible = coords.bottom > 0;
+  return bottomVisible;
+}
+
+window.addEventListener("scroll", () => {
+  if (isVisible($carouselLogo) && logoVisibility === false) {
+    $headerLogo.classList.remove("show");
+    $navbar.classList.remove("thin");
+    logoVisibility = true;
+    return;
+  }
+  if (!isVisible($carouselLogo) && logoVisibility === true) {
+    $headerLogo.classList.add("show");
+    $navbar.classList.add("thin");
+    logoVisibility = false;
+  }
+});
