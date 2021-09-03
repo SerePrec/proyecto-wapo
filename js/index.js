b2b534ce59bda3aef834223ec9a2952319c75a64
Sergio E. Prellezo <sereprec@gmail.com>
2021-09-03 18:29:35 -0300

    Barra blanca header mobile

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const $carouselLogo = document.getElementById("carousel__logo");
 const $headerLogo = document.getElementById("header__logo");
 const $navbar = document.querySelector("header nav.nav");
+const $buttonBar = document.querySelector("header .button-bar");
 
 let logoVisibility = isVisible($carouselLogo);
 
@@ -15,12 +16,14 @@ window.addEventListener("scroll", () => {
   if (isVisible($carouselLogo) && logoVisibility === false) {
     $headerLogo.classList.remove("show");
     $navbar.classList.remove("thin");
+    $buttonBar.classList.remove("show");
     logoVisibility = true;
     return;
   }
   if (!isVisible($carouselLogo) && logoVisibility === true) {
     $headerLogo.classList.add("show");
     $navbar.classList.add("thin");
+    $buttonBar.classList.add("show");
     logoVisibility = false;
   }
 });
